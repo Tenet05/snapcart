d58aa43e529c06271f774b4902a7114e235a7719
Tenet <testmail.services05@gmail.com>
2026-05-07 20:01:54 +0530

    Email error logs fixed 3

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,23 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  family: 4, // FORCE IPv4
-
-  tls: {
-    rejectUnauthorized: false,
-  },
-
-  connectionTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
