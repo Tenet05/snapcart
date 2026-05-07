869549b0c8986c7f17f1aeb539e9019b0b6c7311
Tenet <testmail.services05@gmail.com>
2026-05-07 20:42:36 +0530

    switched email provider to Brevo

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
