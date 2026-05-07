684614f54e778b683db1274a10b0831055b1acb9
Tenet <testmail.services05@gmail.com>
2026-05-07 17:25:37 +0530

    Added default route

--- server/server.js
+++ server/server.js
@@ -46,8 +46,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Snap Cart Backend API is Running");
+});
 
 // Start server
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
+
